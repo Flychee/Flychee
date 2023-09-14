1bd2ed2eadf6e9d61dc5e8ef51832ba620bf15db
Flychee <1229853676@qq.com>
2023-09-14 18:21:06 +0800

    读取文件函数

--- 3121004843/paper_check/main.py
+++ 3121004843/paper_check/main.py
@@ -5,5 +5,12 @@ import re
 
 # 清除Html
 def clean(file):
-    result = re.compile(r'<[^>]+>', re.S).sub('', html).strip()
+    result = re.compile(r'<[^>]+>', re.S).sub('', file).strip()
+    return result
+
+
+# 读取文件
+def read_file(file):
+    with open(file, 'w') as text:
+        result = clean(text.read())
     return result
