81a0f97f943469fc624dea3c76852d597b298adb
Flychee <1229853676@qq.com>
2023-09-25 17:52:11 +0800

    表达式生成

--- 3121004843/Pair_Project/main.py
+++ 3121004843/Pair_Project/main.py
@@ -109,7 +109,7 @@ def evaluate(Tree):
     rightC = Tree.getRightChild()
     if leftC and rightC:
         fn = opers[Tree.getRootVal()]
-        return fn(evaluate(leftC), evaluate(rightC))
+        return fn(Fraction(evaluate(leftC)), Fraction(evaluate(rightC)))
     else:
         return Tree.getRootVal()
 
@@ -133,16 +133,83 @@ def check(tree1, tree2):
             return False
     elif tree1 and tree2:
         if tree1.key == tree2.key:
-            return (check(tree1.leftChild, tree2.leftChild) and check(tree1.rightChild, tree2.rightChild)) or\
+            return (check(tree1.leftChild, tree2.leftChild) and check(tree1.rightChild, tree2.rightChild)) or \
                    (check(tree1.leftChild, tree2.rightChild) and check(tree1.rightChild, tree2.leftChild))
         else:
             return False
 
 
+# pt1 = buildTree("( 3 + ( 1 + 2 )  * 2 )")
+# pt2 = buildTree("( ( 2 + 1 ) + 3 )")
 
-pt1 = buildTree("( 3 + ( 1 + 2 ) )")
-pt2 = buildTree("( ( 2 + 1 ) + 3 )")
 
-print(check(pt1, pt2))
-print(evaluate(pt1))
-print(Fraction_conversion(Fraction(evaluate(pt2))))
+# print(check(pt1, pt2))
+# print(evaluate(pt1))
+
+
+# print(Fraction_conversion(Fraction(evaluate(pt2))))
+
+# 表达式计算顺序确定
+def cal_seq(expression):
+    left = ['(']
+    right = [')']
+    temp = expression.split(' ')
+    result = expression.split(' ')
+    # 添加括号
+    if len(temp) == 5:
+        rad = random.random()
+        if rad < 0.5:
+            result = left + temp[0: 3] + right + temp[3:]
+        else:
+            result = temp[0: 2] + left + temp[2:] + right
+    if len(temp) == 7:
+        rad = random.random()
+        if rad < 1.0 / 3.0:
+            result = left + left + temp[0: 3] + right + temp[3: 5] + right + temp[5:]
+        elif 1.0 / 3.0 <= rad < 2.0 / 3.0:
+            result = temp[0: 2] + left + temp[2: 4] + left + temp[4:] + right + right
+        else:
+            result = left + temp[0: 3] + right + [temp[3]] + left + temp[4:] + right
+    return ' '.join(left + result + right)
+
+
+# 字符串生成
+def question(argv_n, argv_r):
+    if argv_r <= 0 or argv_n <= 0:
+        raise ValueError
+    else:
+        oper = ['+', '-', '*', '/']
+        num_random = random.randrange
+        result = []
+        num = 1
+        while num <= argv_n:
+            temp = str(num_random(1, argv_r))
+            oper_num = num_random(1, 4)
+            point = False
+            for on in range(oper_num):
+                ro = random.choice(oper)
+                nr = num_random(1, argv_r)
+                if ro == '-':
+                    if int(temp[-1]) < nr:
+                        point = True
+                    else:
+                        temp = temp + ' ' + ro + ' ' + str(nr)
+                else:
+                    temp = temp + ' ' + ro + ' ' + str(nr)
+
+            if not point:
+                temp_result = cal_seq(temp)
+                add_point = True
+                now = buildTree(temp_result)
+                if num != 1:
+                    for r in result:
+                        before = buildTree(r)
+                        add_point = add_point and (not check(before, now))
+                if add_point:
+                    num += 1
+                    result.append(temp_result)
+                    # print(temp_result)
+                    # print(Fraction_conversion((evaluate(now))))
+        return result
+
+# question(20, 40)
